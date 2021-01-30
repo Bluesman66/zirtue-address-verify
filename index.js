5e80213e44a58d36c1d9f90aef886fd888b8d81d
Alex Paclin <alex.paclin@gmail.com>
2021-01-30 19:54:48 +0500

    works

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+const colors = require('colors');
 const config = require('./config.json');
 const mongoose = require('mongoose');
 const axios = require('axios');
 const BusinessPartner = require('./models/business_partner');
 const Customer = require('./models/customer');
+
+colors.setTheme({
+	info: 'bgBlue',
+	warn: 'bgCyan',
+	success: 'bgGreen',
+	error: 'bgRed',
+});
+
+const client = axios.default.create({
+	baseURL: 'https://uat-api.synapsefi.com/v3.1',
+	headers: {
+		'X-SP-GATEWAY': `${config.clientId}|${config.clientSecret}`,
+		'X-SP-USER': '|static_pin',
+		'X-SP-USER-IP': '127.0.0.1',
+		'Content-Type': 'application/json',
+	},
+});
 
 const DEF_DELAY = 100;
 const DEF_ATTEMPTS = 3;
@@ -10,25 +28,21 @@ const DEF_ATTEMPTS = 3;
 (async function () {
 	try {
 		await mongoose.connect(config.mongodb_uri, { useNewUrlParser: true, useUnifiedTopology: true });
+		startValidate();
 	} catch (e) {
-		(e) => console.error(`Connecting MongoDB Error: ${e.message}`);
+		(e) => console.log(`Connecting MongoDB Error: ${e.message}`.error);
 		process.exit(1);
 	}
+})();
 
-	const client = axios.default.create({
-		baseURL: 'https://uat-api.synapsefi.com/v3.1',
-		headers: {
-			'X-SP-GATEWAY': `${config.clientId}|${config.clientSecret}`,
-			'X-SP-USER': '|static_pin',
-			'X-SP-USER-IP': '127.0.0.1',
-			'Content-Type': 'application/json',
-		},
-	});
+function startValidate() {
+	validateBusinessPartners();
+}
 
-	const bpCursor = BusinessPartner.find({}).cursor();
+function validateBusinessPartners() {
+	console.log('Validating Business Partners...'.info);
 	validateModel(
-		client,
-		bpCursor,
+		BusinessPartner.find({}).cursor(),
 		(doc) => {
 			const { id, firstName, lastName, address1, city, state, postalCode } = doc;
 			return {
@@ -43,13 +57,16 @@ const DEF_ATTEMPTS = 3;
 			};
 		},
 		(collection) => {
-			console.log(`Business Partners With Invalid Addresses: ${collection.length}`);
+			console.log(`Business Partners With Invalid Addresses: ${collection.length}`.success);
+			validateBusinessPartnersController();
 		}
 	);
+}
 
+function validateBusinessPartnersController() {
+	console.log('Validating Business Partners Controller...'.info);
 	validateModel(
-		client,
-		bpCursor,
+		BusinessPartner.find({}).cursor(),
 		(doc) => {
 			const { id, firstName, lastName, controller } = doc;
 			const { address } = controller;
@@ -66,14 +83,16 @@ const DEF_ATTEMPTS = 3;
 			};
 		},
 		(collection) => {
-			console.log(`Business Partners With Invalid Addresses: ${collection.length}`);
+			console.log(`Business Partners Controller With Invalid Addresses: ${collection.length}`.success);
+			validateCustomers();
 		}
 	);
+}
 
-	const custCursor = Customer.find({}).cursor();
+function validateCustomers() {
+	console.log('Validating Customers...'.info);
 	validateModel(
-		client,
-		custCursor,
+		Customer.find({}).cursor(),
 		(doc) => {
 			const { id, firstName, lastName, address1, city, state, postalCode } = doc;
 			return {
@@ -88,12 +107,13 @@ const DEF_ATTEMPTS = 3;
 			};
 		},
 		(collection) => {
-			console.log(`Customers With Invalid Addresses: ${collection.length}`);
+			console.log(`Customers With Invalid Addresses: ${collection.length}`.success);
+			process.exit(0);
 		}
 	);
-})();
+}
 
-function validateModel(client, cursor, extract, eof) {
+function validateModel(cursor, extract, eof) {
 	const collection = [];
 	let delay = DEF_DELAY;
 	let enumTimerId = setTimeout(function nextDoc() {
@@ -126,21 +146,21 @@ function validateModel(client, cursor, extract, eof) {
 							clearTimeout(verifyTimerId);
 							const { deliverability } = result.data;
 							if (deliverability !== 'usps_deliverable') {
-								collection.push({ id, firstName, lastName });
+								collection.push({ id, firstName, lastName, data: result.data });
 							}
 							console.log(`${firstName} ${lastName} => ${deliverability}`);
 							delay = DEF_DELAY;
 							enumTimerId = setTimeout(nextDoc, delay);
 						})
 						.catch((e) => {
+							clearTimeout(verifyTimerId);
 							if (attempts < DEF_ATTEMPTS) {
 								console.log(`${firstName} ${lastName} => ${e.message}`);
 								attempts++;
 								delay *= 2;
 								verifyTimerId = setTimeout(verifyAdd, delay);
 							} else {
-								collection.push({ id, firstName, lastName });
-								clearTimeout(verifyTimerId);
+								collection.push({ id, firstName, lastName, data: { deliverability: 'error' } });
 								delay = DEF_DELAY;
 								enumTimerId = setTimeout(nextDoc, delay);
 							}
@@ -151,7 +171,6 @@ function validateModel(client, cursor, extract, eof) {
 				if (e.driver) {
 					clearTimeout(enumTimerId);
 					eof(collection);
-					process.exit(0);
 				} else {
 					delay = DEF_DELAY;
 					enumTimerId = setTimeout(nextDoc, delay);
